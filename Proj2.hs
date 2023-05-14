02f509b1bba2f4dc04df73613cd9575fe9fcea44
tutor-1v1 <liumeng64@gmail.com>
2023-05-14 15:13:27 +0800

    WeChat cstutorcs COMP90048 project 2 Haskell

---
+++ Proj2.hs
@@ -0,0 +1,10 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+-- Replace this comment with your opening documentation.  Leave this module declaration as is:
+module Proj2 (feedback, initialGuess, nextGuess, GameState) where
+
+import Card
+
+--Replace this comment with your code....
---
+++ TestCase.hs
@@ -0,0 +1,8 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+module TestCase (testCase) where
+
+testCase :: String
+testCase = "4S QH"
